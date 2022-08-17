2574d545495e4548a94d8d1160239bb8c2073bdf
Emmanuel Odhiambo <emmanuel3odhiambo@gmail.com>
2022-08-18 00:20:13 +0300

    control-flow complete

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){     //if and else if block
+  if (distance <= 400) {
+    return 'This one is on me!';
+  }else if (distance < 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } else if(distance > 2500) {
+      return 'No can do.';
+    }}
+ 
+
+
+
+function ternaryCheckCity(city){                     //using ternary operator
+  return city==='NYC' ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){    //using switch statements 
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  
+  
 }
